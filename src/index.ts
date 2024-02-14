c2abf3d49c40295df2a38e14f51feff7c53a4109
pavitrabhalla <pavitrabhalla@gmail.com>
2024-02-14 01:53:18 -0800

    script runs and populates the DB properly

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,51 @@
-import express from 'express';
-import morgan from "morgan";
+// import express from 'express';
+// import morgan from "morgan";
+import db from "./modules/db";
 import Parser from "rss-parser";
-
-const app = express();
-app.use(morgan('dev'));
+import * as console from "console";
 
 const parser = new Parser();
+async function getFeeds() {
+    return await db.feed.findMany();
+}
 
-parser.parseURL('https://rss.art19.com/masters-of-scale')
-    .then(feed => {
-        feed.items.forEach(async (item) => {
-            console.log(item.title);
-            console.log(item.link);
-        });
+getFeeds().then((feed) => {
+    feed.forEach((f) => {
+        parser.parseURL(f.link)
+            .then(feed => {
+                feed.items.forEach(async (item) => {
+                    db.episode.findFirst({
+                        where: {
+                            title: item.title,
+                            feedId: f.feedId
+                        }
+                    }).then(async (result) => {
+                        if (result === null) {
+                            console.log("Creating new episode: " + item.title);
+                            await db.episode.create({
+                                data: {
+                                    title: item.title ? item.title : '',
+                                    link: item.link,
+                                    publishedAt: item.pubDate ? new Date(item.pubDate) : new Date(),
+                                    description: item.description,
+                                    feedId: f.feedId
+                                }
+                            });
+                        }
+                    });
+                });
+            });
     });
-
-app.get('/', (req, res) => {
-    res.json({'hello': 'world'});
 });
 
-const port = Number(process.env.PORT) || 8080;
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on port http://localhost:${port}`);
-})
+// const app = express();
+// app.use(morgan('dev'));
+//
+// app.get('/', (req, res) => {
+//     res.json({'hello': 'world'});
+// });
+//
+// const port = Number(process.env.PORT) || 8080;
+// app.listen(port, '0.0.0.0', () => {
+//     console.log(`Server is running on port http://localhost:${port}`);
+// })
